12a5edc0436b231a2f4e859482551e8c861fb827
grace <grace.c.youngblood@gmail.com>
2021-07-31 13:15:50 -0400

    Add `gem 'administrate'`.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :bank_cards
+      resources :sellers
+      resources :purchases
+      resources :companies
+      resources :bids
+      resources :buyers
+
+      root to: "purchases#index"
+    end
   root to: "pages#index"
 end
